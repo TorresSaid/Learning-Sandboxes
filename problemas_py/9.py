14ac0124e372d2177c5064d532fbfd8e0f4ba7b3
TorresSaid <fatm479@gmail.com>
2026-05-14 23:59:23 -0500

    Diccionario

---
+++ problemas_py/9.py
@@ -0,0 +1,10 @@
+# Diccionario — cada valor tiene una etiqueta
+persona = {
+    "nombre": "Said",
+    "edad": 20,
+    "ciudad": "Lima",
+    "estudia": "si"
+}
+
+print(persona["nombre"])   # Said
+print(persona["edad"])     # 20
